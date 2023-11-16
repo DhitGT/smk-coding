b239a9672778537e5045a32e90d174fea879b987
DhitGT <adityadwisaputra666@gmail.com>
2023-11-16 19:16:34 +0700

    menambahkan fitur create category ke supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,18 +3,29 @@ export default defineNuxtConfig({
   app: {
     head: {
       link: [
-        { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css"}
+        {
+          rel: "stylesheet",
+          href: "https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css",
+        },
       ],
-    }
+    },
   },
-  
+  appConfig: {
+    apikey: process.env.SUPABASE_API_KEYS,
+    baseUrl: process.env.SUPABASE_URL,
+    secretKey: process.env.SUPABASE_SECRETS_KEY,
+    storageUrl: process.env.SUPABASE_STORAGE_URL,
+    baseStorageUrl: process.env.SUPABASE_GET_STORAGE_URL,
+  },
+
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@vite-pwa/nuxt'],
+
+  modules: ["@nuxtjs/tailwindcss", "@vite-pwa/nuxt", "@pinia/nuxt"],
   tailwindcss: {
     cssPath: "~/assets/css/tailwind.css",
     configPath: "~/tailwind.config.ts",
   },
-  
+
   pwa: {
     manifest: {
       name: "smkcoding",
@@ -29,22 +40,22 @@ export default defineNuxtConfig({
         {
           src: "images/icons/icon-96x96.png",
           sizes: "96x96",
-          type: "image/png"
+          type: "image/png",
         },
         {
           src: "images/icons/icon-128x128.png",
           sizes: "128x128",
-          type: "image/png"
+          type: "image/png",
         },
         {
           src: "images/icons/icon-144x144.png",
           sizes: "144x144",
-          type: "image/png"
+          type: "image/png",
         },
-      ]
+      ],
     },
     devOptions: {
       enabled: true,
-    }
-  }
-})
+    },
+  },
+});
--- pages/category/create.vue
+++ pages/category/create.vue
@@ -1,7 +1,16 @@
+<script lang="ts" setup>
+const categoryStore = useCategoryStore();
+
+let result;
+result = "{'name':'topi','description' : 'khusus topi'}";
+await categoryStore.createCategory(result);
+if (!categoryStore.status) {
+}
+</script>
 <template>
-<section>
-<div class="container">
-<FormsCategoryForm />
-</div>
-</section>
+  <section>
+    <div class="container">
+      <FormsCategoryForm />
+    </div>
+  </section>
 </template>
